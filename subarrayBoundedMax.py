b5458f7d2cd520e3af3f19eda40401abc81f8598
Amar Vashishth <vashishth.amar@gmail.com>
2021-06-17 18:27:16 +0530

    more code

---
+++ subarrayBoundedMax.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+  def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+    ans = 0
+    
+    return ans
+
+nums = [2, 1, 4, 3]
+left = 2
+right = 3
+Solution().numSubarrayBoundedMax(nums, left, right)
