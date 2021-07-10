250b8497361cb2085f82beda4ec531a4e364794e
Amar Vashishth <vashishth.amar@gmail.com>
2021-07-10 21:00:03 +0530

    about to update environment

---
+++ capacityToShip.py
@@ -0,0 +1,7 @@
+
+
+
+
+weights = [1,2,3,4,5,6,7,8,9,10]
+days = 5
+Solution().numDecodings(weights)
---
+++ decodeWays2.py
@@ -0,0 +1,30 @@
+from rich.console import Console
+from rich.traceback import install
+from rich import inspect
+install()
+console = Console()
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        MOD = 10**9 + 7
+        e0 = 1
+        e1 = 0
+        e2 = 0
+        inspect(s)
+        for c in s:
+            if c == '*':
+                f0 = 9*e0 + 9*e1 + 6*e2
+                f1 = e0
+                f2 = e0
+            else:
+                f0 = (c > '0')*e0 + e1 + (c <= '6')*e2
+                f1 = (c == '1')*e0
+                f2 = (c == '2')*e0
+            # console.log(log_locals=True)
+            e0, e1, e2 = f0 % MOD, f1, f2
+        
+        return e0
+
+
+s = "2*1"
+Solution().numDecodings(s)
