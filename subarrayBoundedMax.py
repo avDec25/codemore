625660418347646f1a11c4480ad45f7039fa75c8
Amar Vashishth <vashishth.amar@gmail.com>
2021-08-29 13:03:33 +0530

    from ubuntu initial commit

--- subarrayBoundedMax.py
+++ subarrayBoundedMax.py
@@ -2,8 +2,17 @@ from typing import List
 
 class Solution:
   def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-    ans = 0
-    
+    ans, dp = 0, 0
+    prev = -1
+    for i in range(len(nums)):
+      if nums[i] < left:
+        pass
+      if right < nums[i]:
+        dp = 0
+        prev = i
+      if left <= nums[i] <= right:
+        dp = i - prev
+      ans += dp
     return ans
 
 nums = [2, 1, 4, 3]
